4f3ce24c80768ce100e1e7b3750d5993d5bc3e69
Kyle Hebert <kyle.hebert@gmail.com>
2017-07-06 22:49:42 -0500

    Add Function for Rewarding Adventurers and Updating Player Inventory

--- app.js
+++ app.js
@@ -1,39 +1,78 @@
+ var game = {
+    maxQuests: 3,
+    activeQuests: [],
+    completedQuests: [],
+    player: {
+        inventory: {
+            ratTails: 0,
+            goblinEars: 0,
+            harpyFeathers:0
+        }
+    },
+    rewards: {
+        levelOneReward: 5,
+        levelTwoReward: 3,
+        levelThreeReward: 2, 
+    }
+ } 
 
-var activeQuests = []
-var completedQuests = []
+
+
+
+
 
 function $(id) {
     return document.getElementById(id)
 }
 
 function giveQuest(e) {
-    activeQuests.push(e.srcElement.id)
+    game.activeQuests.push(e.srcElement.id)
     $(e.target.id).classList.add("disabled")
-    $("activeQuests").innerHTML = "Active Quests: " + activeQuests.length + "/3"
+    $("activeQuests").innerHTML = "Active Quests: " + game.activeQuests.length + "/" + game.maxQuests
 }
 
 function rewardQuest(e) {
-
+    const reward = e.target.id
+    $(reward).classList.add("disabled")
+    switch(reward) {
+        case "levelOneReward":
+            game.player.inventory.ratTails += game.rewards[e.target.id]
+            $("ratTails").innerHTML = "Rat Tails: " +  game.player.inventory.ratTails
+            $("levelOne").classList.remove("disabled")
+            $("levelOneProgress").firstChild.style.width = "0%"
+            break
+        case "levelTwoReward":
+            game.player.inventory.goblinEars += game.rewards[e.target.id]
+            $("goblinEars").innerHTML = "Goblin Ears: " + game.player.inventory.goblinEars
+            $("levelTwo").classList.remove("disabled")
+            $("levelTwoProgress").firstChild.style.width = "0%"
+            break
+        default:
+            game.player.inventory.harpyFeathers += game.rewards[e.target.id]
+            $("harpyFeathers").innerHTML = "Harpy Feathers: " + game.player.inventory.harpyFeathers
+            $("levelThree").classList.remove("disabled")
+            $("levelThreeProgress").firstChild.style.width = "0%"
+    }
 }
 
 function readyQuest() {
-    completedQuests.forEach(function(quest) {
+    game.completedQuests.forEach(function(quest) {
         $(quest + "Reward").classList.remove("disabled")
-        completedQuests = completedQuests.filter(q => q !== quest)
+        game.completedQuests = game.completedQuests.filter(q => q !== quest)
     });
 }
 
 function progressQuest() {
     // for each quest in quest array
-    activeQuests.forEach(function(quest) {
+    game.activeQuests.forEach(function(quest) {
         progressBar = $(quest +"Progress").firstChild
         progress = progressBar.style.width.split("%")
         progressValue = Number(progress[0]) +1 * 10
         if (progressValue <= 100) {
             progressBar.style.width = progressValue.toString() + "%"
         } else {
-            activeQuests = activeQuests.filter(q => q !== quest)
-            completedQuests.push(quest)
+            game.activeQuests = game.activeQuests.filter(q => q !== quest)
+            game.completedQuests.push(quest)
         }
     });
 }
