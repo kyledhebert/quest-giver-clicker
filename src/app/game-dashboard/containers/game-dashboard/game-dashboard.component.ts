5738eb204b3cb2079907dee777413ca8059eef1b
kylehebert <kyle.hebert@gmail.com>
2017-08-19 22:54:02 -0500

    Add Game Service to Game Dashboard Component

--- src/app/game-dashboard/containers/game-dashboard/game-dashboard.component.ts
+++ src/app/game-dashboard/containers/game-dashboard/game-dashboard.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { GameDashboardService } from '../../game-dashboard.service';
 
 @Component({
     selector: 'app-game-dashboard',
     styleUrls: ['game-dashboard.component.css'],
     templateUrl: './game-dashboard.component.html'
 })
-export class GameDashboardComponent {}
+export class GameDashboardComponent implements OnInit {
+
+    constructor(private service: GameDashboardService) {}
+    ngOnInit() {
+        setInterval(() => {
+            this.service.progressQuest();
+        }, 1000);
+    }
+}
