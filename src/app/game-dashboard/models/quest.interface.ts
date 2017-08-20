17fb8deafd81d5fe6c5826713b7081c173062134
kylehebert <kyle.hebert@gmail.com>
2017-08-19 22:53:17 -0500

    Add ID and Button States to Quest Interface

--- src/app/game-dashboard/models/quest.interface.ts
+++ src/app/game-dashboard/models/quest.interface.ts
@@ -1,7 +1,10 @@
 export interface Quest {
+    id: number;
     name: string;
     turnIn: string;
     turnInQty: number;
     text: string;
     completion: number;
+    giveButtonDisabled: boolean;
+    rewardButtonDisabled: boolean;
 }
