8d3f433dd92422d816d7fbc1d30ecfd93ead2ae9
kylehebert <kyle.hebert@gmail.com>
2017-08-23 22:08:12 -0500

    Add Giving and Rewarding Quest to Game Service

--- src/app/game-dashboard/game-dashboard.service.ts
+++ src/app/game-dashboard/game-dashboard.service.ts
@@ -1,9 +1,69 @@
 import { Injectable } from '@angular/core';
 
 import { Quest } from './models/quest.interface';
+import { InventoryItem } from './models/inventory-item.interface';
 
 @Injectable()
 export class GameDashboardService {
+
+  inventory: InventoryItem[] = [
+    {id: 1, name: 'Rat Tails', qty: 0, basePrice: 1},
+    {id: 2, name: 'Goblin Ears', qty: 0, basePrice: 2},
+    {id: 3, name: 'Kobold Candles', qty: 0, basePrice: 3}
+  ];
+
+  quests: Quest[] = [
+    {
+      id: 1,
+      name: 'Level One Quest',
+      text: 'This is a level one quest',
+      turnIn: 'rat tails',
+      turnInQty: 10,
+      completion: 50,
+      giveButtonDisabled: false,
+      rewardButtonDisabled: true,
+      isActive: false,
+      isComplete: false
+    },
+    {
+      id: 2,
+      name: 'Level Two Quest',
+      text: 'This is a level one quest',
+      turnIn: 'rat tails',
+      turnInQty: 10,
+      completion: 0,
+      giveButtonDisabled: false,
+      rewardButtonDisabled: true,
+      isActive: false,
+      isComplete: false
+    },
+    {
+      id: 3,
+      name: 'Level Three Quest',
+      text: 'This is a level one quest',
+      turnIn: 'rat tails',
+      turnInQty: 10,
+      completion: 0,
+      giveButtonDisabled: false,
+      rewardButtonDisabled: true,
+      isActive: false,
+      isComplete: false
+    },
+    {
+      id: 4,
+      name: 'Level Three Quest',
+      text: 'This is a level one quest',
+      turnIn: 'rat tails',
+      turnInQty: 10,
+      completion: 0,
+      giveButtonDisabled: false,
+      rewardButtonDisabled: true,
+      isActive: false,
+      isComplete: false
+    }
+  ];
+
+
 
   activeQuests: Quest[] = [];
   completedQuests: Quest[] = [];
@@ -11,67 +71,41 @@ export class GameDashboardService {
   constructor() {}
 
   getQuests(): Quest[] {
-    return [
-      {
-        id: 1,
-        name: 'Level One Quest',
-        text: 'This is a level one quest',
-        turnIn: 'rat tails',
-        turnInQty: 10,
-        completion: 50,
-        giveButtonDisabled: false,
-        rewardButtonDisabled: true
-      },
-      {
-        id: 2,
-        name: 'Level Two Quest',
-        text: 'This is a level one quest',
-        turnIn: 'rat tails',
-        turnInQty: 10,
-        completion: 0,
-        giveButtonDisabled: false,
-        rewardButtonDisabled: true
-      },
-      {
-        id: 3,
-        name: 'Level Three Quest',
-        text: 'This is a level one quest',
-        turnIn: 'rat tails',
-        turnInQty: 10,
-        completion: 0,
-        giveButtonDisabled: false,
-        rewardButtonDisabled: true
-      },
-      {
-        id: 4,
-        name: 'Level Three Quest',
-        text: 'This is a level one quest',
-        turnIn: 'rat tails',
-        turnInQty: 10,
-        completion: 0,
-        giveButtonDisabled: false,
-        rewardButtonDisabled: true
-      }
-    ];
+    console.log(this.quests);
+    return this.quests;
+  }
+
+  saveQuests(quests: Quest[]) {
+    this.quests = quests;
   }
 
   giveQuest(quest: Quest) {
-    console.log(quest);
-    this.activeQuests.push(quest);
-    console.log(this.activeQuests);
+    quest.giveButtonDisabled = true;
+    quest.isActive = true;
   }
 
   progressQuest() {
-    this.activeQuests.forEach( quest => {
-      if (quest.completion < 100) {
+    this.quests.forEach(quest => {
+      if ((quest.isActive) && quest.completion < 100) {
         quest.completion += (1 * 10);
-        console.log(quest.completion);
-      } else {
+      } else if (quest.completion === 100) {
         quest.rewardButtonDisabled = false;
-        this.activeQuests = this.activeQuests.filter(q => q.id !== quest.id);
-        this.completedQuests.push(quest);
+        quest.isComplete = true;
+        quest.isActive = false;
       }
     });
   }
 
+  rewardQuest(quest: Quest) {
+    quest.rewardButtonDisabled = true;
+    quest.giveButtonDisabled = false;
+    quest.completion = 0;
+    quest.isComplete = false;
+    this.updateInventory(quest);
+  }
+
+  updateInventory(quest: Quest) {
+    const item = this.inventory.find(i => i.name.toLocaleLowerCase === quest.turnIn.toLocaleLowerCase);
+    item.qty += quest.turnInQty;
+  }
 }
